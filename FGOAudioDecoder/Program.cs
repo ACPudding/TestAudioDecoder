c27f4d65cf06a61290a0f513c1b70e6a278c61c7
ACPudding <miaomiao2130@gmail.com>
2020-12-06 02:20:35 +0800

    Update Files. Under construction.

--- FGOAudioDecoder/Program.cs
+++ FGOAudioDecoder/Program.cs
@@ -1,15 +1,122 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FGOAudioDecoder
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task DisplayMenu()
         {
+            Console.Clear();
+            try
+            {
+                int arg;
+                Console.WriteLine(
+                    "------FGOAudioDecoder------\n" +
+                    "1: cpk2wav(单文件)\t" +
+                    "2: cpk2wav(选择文件夹批量转换) - 待写\n" +
+                    "3: Usm2(m2v+wav)(单文件)\t" +
+                    "4: Usm2(m2v+wav)(选择文件夹批量转换) - 待写\n" +
+                    "999: 退出程序\n" +
+                    "请选择功能..."
+                );
+                try
+                {
+                    arg = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    arg = -1;
+                }
+
+                switch (arg)
+                {
+                    case 1:
+                        Console.WriteLine("请将cpk文件拖入窗口内获取路径,并按回车键:");
+                        var filepath = Console.ReadLine();
+                        var file = new FileInfo(filepath);
+                        var outputfolder = file.DirectoryName;
+                        var output = new DirectoryInfo(outputfolder);
+                        await Task.Run(async () => { await DecryptCpkFile(file, output); });
+                        Thread.Sleep(1000);
+                        Console.WriteLine("解包完成,点击任意键继续...");
+                        Console.ReadKey(true);
+                        await DisplayMenu();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Console.WriteLine("请将Usm文件拖入窗口内获取路径,并按回车键:");
+                        var filepathusm = Console.ReadLine();
+                        var fileusm = new FileInfo(filepathusm);
+                        var outputfolderusm = fileusm.DirectoryName;
+                        var outputusm = new DirectoryInfo(outputfolderusm);
+                        FGOAudioDecoder.DecodeUsmFiles(fileusm, outputusm);
+                        Thread.Sleep(1000);
+                        Console.WriteLine("解包完成,点击任意键继续...");
+                        Console.ReadKey(true);
+                        await DisplayMenu();
+                        break;
+                    case 4:
+                        break;
+                    case 999:
+                        return;
+                    default:
+                        Console.WriteLine("请输入一个有效选项,点击任意键重新选择...");
+                        Console.ReadKey(true);
+                        await DisplayMenu();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("遇到错误,点击任意键重新开始...");
+                Console.ReadKey(true);
+                await DisplayMenu();
+            }
+        }
+
+        private static async Task DecryptCpkFile(FileInfo file, DirectoryInfo outputfolder)
+        {
+            Console.WriteLine("解包音频: " + file.Name);
+            var acbFilename = "";
+            await Task.Run(() =>
+            {
+                try
+                {
+                    acbFilename = FGOAudioDecoder.UnpackCpkFiles(file, outputfolder);
+                }
+                catch (Exception)
+                {
+                    //ignore
+                }
+            });
+            if (acbFilename == "")
+            {
+                Console.WriteLine("该文件不是cpk类型文件.请重试.");
+                return;
+            }
+
+            await Task.Run(() => { FGOAudioDecoder.DecodeAcbFiles(new FileInfo(acbFilename), outputfolder); });
+            Thread.Sleep(1500);
+            Process.Start(outputfolder.FullName + @"\DecodedWavs");
+        }
+
+        private static async Task Main(string[] args)
+        {
+            var path = Directory.GetCurrentDirectory();
+            if (!File.Exists(path + @"\crid.exe"))
+                ExtractExe.ExtractResFile("FGOAudioDecoder.Application.crid.exe", path + @"\crid.exe");
+            if (!File.Exists(path + @"\ffmpeg.exe"))
+                ExtractExe.ExtractResFile("FGOAudioDecoder.Application.ffmpeg.exe", path + @"\ffmpeg.exe");
+            await DisplayMenu();
+            Console.WriteLine("点按任意键退出...");
+            Console.ReadKey(true);
         }
     }
 }
